0e4f2c1b94a8277969f66313f4807ffb2d58218f
Abhishek <lodhi20abhishek@gmail.com>
2021-08-31 23:46:55 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,42 @@
+
+
+let allMusic = [
+  {
+    name: "Shawn Mendes - Senorita",
+    artist: "Shawn Mendes",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Diljit Dosanjh - Raat Di Gedi",
+    artist: "Diljit Dosanjh",
+    img: "music-7",
+    src: "music-7"
+  },
+ 
+
+];
